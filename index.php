39aa916d0d15eb4e49eff516eafe149f40cfd85a
Esteban Guzmán <57820858+ebanguz@users.noreply.github.com>
2020-10-29 10:59:26 -0400

    todo setiado para empezar con el layout y las funciones del printHeader y printFooter

---
+++ index.php
@@ -0,0 +1,73 @@
+<main role="main">
+
+    <section class="jumbotron text-center">
+        <div class="container">
+            <h1>Integrar estudiante</h1>
+            <!-- <p class="lead text-muted">Something short and leading about the collection below—its contents, the
+                    creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it
+                    entirely.</p>-->
+            <p>
+                <a href="#" class="btn btn-primary my-2">Añadir</a>
+            </p>
+        </div>
+    </section>
+
+    <div class="album py-5 bg-light">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-4 shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
+                            xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"
+                            role="img" aria-label="Placeholder: Thumbnail">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
+                                dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<footer class="text-muted">
+    <div class="container">
+        <p class="float-right">
+            <a href="#">Volver arriba</a>
+        </p>
+        <p>EGA</p>
+
+    </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+
+<script src="assets/js/bootstrap.min.js">
+</script>
+
+<div id="weava-permanent-marker" date="1603938382626"></div>
+<div id="weava-ui-wrapper">
+    <div class="weava-drop-area-wrapper">
+        <div class="weava-drop-area"></div>
+        <div class="weava-drop-area-text">Drop here!</div>
+    </div>
+</div>
+</body>
+
+</html>
